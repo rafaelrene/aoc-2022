7ca55b11c66304891b8beef91920f35d950509a5
René Rafael <rrafael@groupsolver.com>
2023-01-16 23:06:42 +0100

    [DAY 04]: Prepared data

---
+++ src/bin/04.rs
@@ -0,0 +1,43 @@
+fn parse_input(input: &str) -> Vec<&str> {
+    input
+        .split("\n")
+        .filter(|round| !round.is_empty())
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let _parsed_input = parse_input(input);
+
+    println!("{:?}", _parsed_input);
+
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let _parsed_input = parse_input(input);
+
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 4);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_two(&input), None);
+    }
+}
